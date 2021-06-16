ea6906cdd0fb8a9a02fcd0a17aac603df37eb3f5
Bento Borges Schirmer <bento.schirmer@gmail.com>
2021-06-16 10:50:31 -0300

    Made tests using strings

--- Main.hs
+++ Main.hs
@@ -2,8 +2,13 @@
 
 module Main where
 
-import Foreign.C.Types
+-- Reference: https://hackage.haskell.org/package/base/docs/Foreign-C-Types.html
+--            https://hackage.haskell.org/package/base/docs/Foreign-C-String.html
 
+import Foreign.C.Types
+import Foreign.C.String
+
+-- Thanks https://stackoverflow.com/a/57495181
 main :: IO ()
 main = return ()
 
@@ -14,4 +19,11 @@ fibonacci n = fibs !! n
 fibonacci_hs :: CInt -> CInt
 fibonacci_hs = fromIntegral . fibonacci . fromIntegral
 
+string_test :: String
+string_test = "Hello world, from Haskell inside C"
+
+string_test_hs :: IO CString
+string_test_hs = newCString string_test
+
 foreign export ccall fibonacci_hs :: CInt -> CInt
+foreign export ccall string_test_hs :: IO CString
--- main.c
+++ main.c
@@ -2,7 +2,9 @@
 #ifdef __GLASGOW_HASKELL__
 #include "Main_stub.h"
 #endif
+
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
@@ -11,6 +13,10 @@ int main(int argc, char *argv[])
 
 	i = fibonacci_hs(42);
 	printf("Fibonacci: %d\n", i);
+
+	char *string = string_test_hs();
+	printf("%s\n", string);
+	free(string);
 
 	hs_exit();
 	return 0;
