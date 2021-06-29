5b7346bf01a071430956db107d77eb7896c58a21
Bento Borges Schirmer <bento.schirmer@gmail.com>
2021-06-29 18:05:16 -0300

    Added polyline element creator and list of points to String converter

--- Main.hs
+++ Main.hs
@@ -1,5 +1,13 @@
 import Text.Printf
+import Data.List
 
-genSvgBody :: Float -> Float -> [String] -> String
-genSvgBody height width children =
-  printf "<svg height=\"%.3f\" width=\"%.3f\">%s</svg" height width (concat children)
+writeSvgBody :: Float -> Float -> [String] -> String
+writeSvgBody height width children =
+  printf "<svg height=\"%.3f\" width=\"%.3f\">%s</svg>" height width (concat children)
+
+writePolyline :: String -> [(Float, Float)] -> String
+writePolyline style points =
+  printf "<polyline points=\"%s\" style=\"%s\" />" (writePoints points) style
+
+writePoints :: [(Float, Float)] -> String
+writePoints = intercalate " " . map (\(x,y) -> printf "%.3f,%.3f" x y)
