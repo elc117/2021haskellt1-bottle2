971a7dda7b35f802c3682329746fc6ed474a1a26
Bento Borges Schirmer <bento.schirmer@gmail.com>
2021-06-29 19:28:57 -0300

    Drawing a circle

--- Main.hs
+++ Main.hs
@@ -1,9 +1,14 @@
 import Text.Printf
 import Data.List
 
+
+radians :: Float -> Float
+radians = (* pi) . (/ 180)
+
+
 writeSvgBody :: Float -> Float -> [String] -> String
 writeSvgBody height width children =
-  printf "<svg height=\"%.3f\" width=\"%.3f\">%s</svg>" height width (concat children)
+  printf "<svg height=\"%.3f\" width=\"%.3f\" xmlns=\"http://www.w3.org/2000/svg\">%s</svg>" height width (concat children)
 
 writePolyline :: String -> [(Float, Float)] -> String
 writePolyline style points =
@@ -11,3 +16,21 @@ writePolyline style points =
 
 writePoints :: [(Float, Float)] -> String
 writePoints = intercalate " " . map (\(x,y) -> printf "%.3f,%.3f" x y)
+
+
+calcCircle :: Float -> Float -> Float -> [(Float, Float)]
+calcCircle offset_x offset_y radius =
+  let points_x = (map ((+offset_x) . (*radius) . sin . radians) [0..360])
+      points_y = (map ((+offset_y) . (*radius) . cos . radians) [0..360])
+  in zip points_x points_y 
+
+main :: IO ()
+main = do
+  putStr $
+    writeSvgBody width height [
+      writePolyline "fill:none;stroke:black;stroke-width:3" (
+        calcCircle (width / 2) (height / 2) 50
+      )
+    ]
+  where width  = 500
+        height = 500
